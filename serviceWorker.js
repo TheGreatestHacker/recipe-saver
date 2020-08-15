1f8ae76676e24e479c6898c1ccd294397ac9f671
Bryan Cruz <bryancruz457@hotmail.com>
2020-08-15 18:59:50 -0500

    Create fetch event to retrieve stored data.

--- serviceWorker.js
+++ serviceWorker.js
@@ -20,13 +20,28 @@ const assets = [
 //The service worker has several life cycles, and one of them is the install event. It runs when a service worker is installed.
 //It's triggered as soon as the worker executes, and it's only called once per service worker.
 //when the install event is fired, we run the callback which gives us access to the event object
+//Caching on the browser can take some time to finish since its asynchronous. To handle this we use waitUntil() which wiats for the action to finish.
+
+//Once the cache API is ready, we can run the open() method and create our cache by pasing its name as an argument to caches.open(staticDevCoffee)
+//return a promise, which helps us store our assets in the cache with cache.addAll(assets)
 self.addEventListener("install", installEvent => {
-  //Caching on the browser can take some time to finish since its asynchronous. To handle this we use waitUntil() which wiats for the action to finish.
   installEvent.waitUntil(
-    //Once the cache API is ready, we can run the open() method and create our cache by pasing its name as an argument to caches.open(staticDevCoffee)
     caches.open(staticDevCoffee).then(cache => {
-      //return a promise, which helps us store our assets in the cache with cache.addAll(assets)
       cache.addAll(assets)
+    })
+  )
+})
+
+//Use the fetch event to get back the data stored.
+//Attach respondWith() to prevent the browser's defualt response and instead returns a promise since the fetch action can take time to finish.
+//Once cache is ready, apply caches.match(...) to check if something int he cache matches.
+//P.S. fetchEvent.request is our array of assets.
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      //returns a promise. 
+      //We can return the result if it exists or the initial fetch if not.
+      return res || fetch(fetchEvent.request)
     })
   )
 })
